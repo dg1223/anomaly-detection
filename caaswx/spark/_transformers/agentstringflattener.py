b260f105f0e24917d11ca7e6ce7f64c114f4ee1e
Ubuntu <nds838@nds838.j5bptqmcza4uvpu5i0ekuzskuh.ux.internal.cloudapp.net>
2021-06-25 14:26:25 +0000

    adding agent string flattner and parser

---
+++ caaswx/spark/_transformers/agentstringflattener.py
@@ -0,0 +1,275 @@
+class UserAgentFlattenerParser(Transformer):
+
+  """
+
+  User Feature transformer for the Streamworx project.
+
+  """
+
+  
+
+  windowLength = Param(
+
+    Params._dummy(),
+
+    "windowLength",
+
+    "Length of the sliding window used for entity resolution. " +
+
+    "Given as an integer in seconds.",
+
+    typeConverter = TypeConverters.toInt
+
+  )
+
+  
+
+  windowStep = Param(
+
+    Params._dummy(),
+
+    "windowStep",
+
+    "Length of the sliding window step-size used for entity resolution. " + 
+
+    "Given as an integer in seconds.",
+
+    typeConverter = TypeConverters.toInt
+
+  )
+
+  
+
+  entityName = Param(
+
+    Params._dummy(),
+
+    "entityName",
+
+    "Name of the column to perform aggregation on, together with the " +
+
+    "sliding window.",
+
+    typeConverter = TypeConverters.toString
+
+  )
+
+  
+
+  agentSizeLimit = Param(
+
+    Params._dummy(),
+
+    "agentSizeLimit",
+
+    "Number of agent strings processed " +
+
+    "Given as the number of strings.",
+
+    typeConverter = TypeConverters.toInt
+
+  )
+
+  
+
+  @keyword_only
+
+  def __init__(self, entityName='SM_USERNAME', agentSizeLimit=5, windowLength = 900, windowStep = 900):
+
+    """
+
+    def __init__(self, *, window_length = 900, window_step = 900)
+
+    """
+
+    super(UserAgentFlattenerParser, self).__init__()
+
+    self._setDefault(windowLength = 900, windowStep = 900, entityName = 'SM_USERNAME', agentSizeLimit=5)
+
+    kwargs = self._input_kwargs
+
+    self.setParams(**kwargs)
+
+
+
+  @keyword_only
+
+  def setParams(self, entityName='SM_USERNAME', agentSizeLimit=5, windowLength = 900, windowStep = 900):
+
+    """
+
+    setParams(self, \\*, threshold=0.0, inputCol=None, outputCol=None, thresholds=None, \
+
+              inputCols=None, outputCols=None)
+
+    Sets params for this UserAgentFlattenerParser.
+
+    """
+
+    kwargs = self._input_kwargs
+
+    return self._set(**kwargs)
+
+  
+
+  
+
+  def setEntityName(self, value):
+
+    """
+
+    Sets the Entity Name
+
+    """
+
+    self._set(entityName = value)
+
+    
+
+  def getEntityName():
+
+    return self.entityName
+
+  
+
+  def setAgentSizeLimit(self, value):
+
+    """
+
+    Sets the Agent Size
+
+    """
+
+    self._set(agentSizeLimit = value)
+
+    
+
+  def getAgentSizeLimit():
+
+    return self.agentSizeLimit
+
+    
+
+  def setWindowLength(self, value):
+
+    """
+
+    Sets this UserAgentFlattenerParser's window length.
+
+    """
+
+    self._set(windowLength = value)
+
+  
+
+  def setWindowStep(self, value):
+
+    """
+
+    Sets this UserAgentFlattenerParser's window step size.
+
+    """
+
+    self._set(windowStep = value)
+
+  
+
+  @staticmethod
+
+  def __flatten(self, df):
+
+    """
+
+    Flattens the URLs based on the order of their occurring timestamps
+
+    Input: Siteminder dataframe
+
+    Output: Dataframe containing entity name, window intervals and flattened URLs combined into lists
+
+    """
+
+    
+
+    # Sorting the dataframe w.r.t timestamps in ascending order
+
+    df = df.sort('SM_TIMESTAMP')
+
+    
+
+    # Applying flattening operation over SM_AGENTNAME by combining them into set for each SM_USERNAME.
+
+    result = (
+
+      df.groupby(
+
+        str(self.getOrDefault('entityName')), 
+
+        window(
+
+          'SM_TIMESTAMP', 
+
+          str(self.getOrDefault('windowLength')) + ' seconds',str(self.getOrDefault('windowStep')) + ' seconds'))
+
+        .agg(f.collect_set('SM_AGENTNAME').alias('SM_AGENTNAME')))
+
+   
+
+    result = result.sort('window')
+
+    # Slicing to only get N User Agent Strings.
+
+    result = result.withColumn('SM_AGENTNAME', f.slice(result['SM_AGENTNAME'], 1,self.getOrDefault('agentSizeLimit')))
+
+    
+
+    return result
+
+  
+
+  def httpParser(self, value):
+
+
+
+    base = []
+
+    for string in value: 
+
+      if (len(string.split(' '))==1):
+
+        if None not in base:
+
+          base.append(None)
+
+      else:
+
+        base.append(httpagentparser.detect(string))
+
+
+
+    return str(base)  
+
+  
+
+  def _transform(self, dataset):
+
+    """
+
+    Overridden function which flattens the input dataset w.r.t URLs
+
+    Input : Siteminder dataframe
+
+    Output : SM_USERNAMEs with flattened URLs merged into lists
+
+    """
+
+    
+
+    result = (self.__flatten(self, dataset))
+
+    
+
+    httpParser_udf = udf(self.httpParser, StringType())
+
+    df = result.withColumn("New_Agent_Name", httpParser_udf(col("SM_AGENTNAME"))).drop('SM_AGENTNAME')
+
+    return df
