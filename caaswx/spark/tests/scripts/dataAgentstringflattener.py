0d410c9d5291b843c419077ae4b66a8feccae711
AndrewGPeng <andrewpoky@gmail.com>
2021-06-28 21:14:48 +0000

    working off starting test case

--- caaswx/spark/tests/scripts/dataAgentstringflattener.py
+++ caaswx/spark/tests/scripts/dataAgentstringflattener.py
@@ -1,7 +1,18 @@
+"""
+Note: not working yet, based off of the main branch's flattener_user_entity.py
+the issue is likely with the typing of the dataframes, timestamp seems to give some problems
 
-from pyspark.context import SparkContext
+"""
+
+
+
 from pyspark.sql.session import SparkSession
-from pyspark.sql.types import StructType,StructField, StringType, ArrayType, TimestampType, MapType
+from pyspark.context import SparkContext
+# from IPython.display import display
+
+import pandas as pd
+
+from pyspark.sql.types import DatetimeConverter, StructType,StructField, StringType, ArrayType, TimestampType, MapType
 from pyspark.sql.functions import to_timestamp, col, shuffle, rand
 
 from agentstringflattener import UserAgentFlattenerParser
@@ -12,16 +23,16 @@ spark = SparkSession(sc)
 test_schema = StructType([ 
         StructField('SM_CLIENTIP', StringType()),
         StructField('SM_AGENTNAME', StringType()),
-        StructField('SM_TIMESTAMP', TimestampType())
+        StructField('SM_TIMESTAMP', StringType())
     ])
 
 ans_schema = StructType([ 
         StructField('SM_CLIENTIP', StringType()),
-        StructField('window_temp', StructType([
-                StructField('start', TimestampType()),
-                StructField('end', TimestampType())
+        StructField('windowtmp', StructType([
+                StructField('start', StringType()),
+                StructField('end', StringType())
             ])),
-        StructField('SM_AGENTNAME', StringType())
+        StructField('New_Agent_Name', StringType())
         
     ])
 
@@ -38,15 +49,33 @@ class agentflattener_datasets:
 
         ans_1_data = [
             ('User_A', 
-            [{'platform': {'name': 'Mac OS', 'version': 'X 10.12.6'}, 'os': {'name': 'Macintosh'}, 'bot': False, 'flavor': {'name': 'MacOS', 'version': 'X 10.12.6'}, 'browser': {'name': 'Chrome', 'version': '63.0.3239.84'}}],
-            {"start":"2018-01-01T18:30:00.000+0000","end":"2018-01-01T18:45:00.000+0000"}
+            {"start": "2018-01-01T18:30:00.000+0000",
+                "end": "2018-01-01T18:45:00.000+0000"},
+            [{'platform': {'name': 'Mac OS', 'version': 'X 10.12.6'}, 'os': {'name': 'Macintosh'}, 'bot': False, 'flavor': {'name': 'MacOS', 'version': 'X 10.12.6'}, 'browser': {'name': 'Chrome', 'version': '63.0.3239.84'}}]
+            
             )
         ]
 
         test_df = spark.createDataFrame(test_1_data, schema=test_schema)
         ans_df = spark.createDataFrame(ans_1_data, schema=ans_schema)
 
+    
+
         result = UserAgentFlattenerParser(agentSizeLimit=2, entityName = 'SM_CLIENTIP').transform(test_df)
         
+
+
+        
+
+        print("yo")
+        print(test_df)
+        print("result: ")
+        print(result)
+        print("ans: ")
+        print(ans_df)
+
         return result, ans_df
         
+data_importer = agentflattener_datasets()
+result, ans_1_data = data_importer.ds1_base()
+
