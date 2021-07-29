acdbc6c0217c245561f4f8d9a37a93641526535b
sagarmoghe5 <16164018+sagarmoghe5@users.noreply.github.com>
2021-07-29 17:03:32 -0300

    Fixed a bug

--- tests/test_cnExtractor.py
+++ tests/test_cnExtractor.py
@@ -4,7 +4,7 @@ from src.caaswx.spark._transformers.cnextractor import CnExtractor
 
 def test_CnExtractor():
     obj = CnExtractor("SM_USERNAME", "CN")
-    testDf, answerDf = DataCnExtractor.generateCnExtractorData()
+    testDf, answerDf = DataCnExtractor().generateCnExtractorData()
     resultDf = obj.transform(testDf)
     sub1 = resultDf.subtract(answerDf)
     sub2 = answerDf.subtract(resultDf)
