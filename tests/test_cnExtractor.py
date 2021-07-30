006e02454591439acc9edd8d88e985b632dabd00
sagarmoghe5 <16164018+sagarmoghe5@users.noreply.github.com>
2021-07-30 12:30:35 -0300

    Testing check

--- tests/test_cnExtractor.py
+++ tests/test_cnExtractor.py
@@ -5,11 +5,8 @@ from src.caaswx.spark._transformers.cnextractor import CnExtractor
 def test_CnExtractor():
     obj = CnExtractor("SM_USERNAME", "CN")
     testDf, answerDf = DataCnExtractor().generateCnExtractorData()
-    testDf.show()
-    answerDf.show()
     resultDf = obj.transform(testDf)
-    resultDf.show()
-    # sub1 = resultDf.subtract(answerDf)
-    # sub2 = answerDf.subtract(resultDf)
+    sub1 = resultDf.subtract(answerDf)
+    sub2 = answerDf.subtract(resultDf)
 
-    # assert bool(sub1.head(1)) is bool(sub2.head(1)) is False
+    assert bool(sub1.head(1)) is bool(sub2.head(1)) is False
