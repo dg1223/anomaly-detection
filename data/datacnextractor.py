7b67c5b4faada22dc2a26a9b7725f6d508647bb9
sagarmoghe5 <16164018+sagarmoghe5@users.noreply.github.com>
2021-07-30 12:34:29 -0300

    Final Commit with bug fixes and testing added

--- data/datacnextractor.py
+++ data/datacnextractor.py
@@ -8,20 +8,6 @@ from pyspark.sql.types import (
 
 class DataCnExtractor:
     def generateCnExtractorData(self):
-        # #     testSchema = StructType([StructField("SM_USERNAME", StringType())])
-        # #     testData = ["cn=hasgdvfashdgfahg,ou=Credential,ou=PR", "asjkdhfbjksdlbaf"]
-        # #     ansSchema = StructType(
-        # #         [StructField("SM_USERNAME", StringType()), StructField("CN", StringType())]
-        # #     )
-        # #     answerData = [
-        # #         ("cn=hasgdvfashdgfahg,ou=Credential,ou=PR", "hasgdvfashdgfahg"),
-        # #         ("asjkdhfbjksdlbaf", "asjkdhfbjksdlbaf"),
-        # #     ]
-        # #     testDf = spark.createDataFrame(testData, schema=testSchema)
-        # #     answerDf = spark.createDataFrame(answerData, schema=ansSchema)
-        #
-        #     return testDf, answerDf
-
         testSchema = StructType(
             [
                 StructField("SM_USERNAME", StringType()),
