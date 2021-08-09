f6daf91595d4ed8b567df64d7a9ae1a200a9145f
AndrewGPeng <andrewpoky@gmail.com>
2021-08-09 21:20:14 +0000

    Added parsing, but will need to test properly. vm being annoying

--- tests/test_agentStringFlattener.py
+++ tests/test_agentStringFlattener.py
@@ -1,17 +1,19 @@
-# from data.dataagentstringflattener import DataAgentStringFlattener
+from src.caaswx.spark._transformers.agentstringflattener import AgentStringFlattener
 from src.caaswx.spark.scripts.loadWriteParquet import loadParquet
 
 # if parser is now working, run test_1, edit code in AgentStringFlattener to activate parser and uncomment block below
 def test_1():
     data_1 = loadParquet(
-        "/home/nds838/Documents/caa-streamworx/data/parquet_data/agent_flattener_tests"
+        "/home/csl122/Documents/caa-streamworx/data/parquet_data/agent_flattener_tests"
         "/test_data_1_agentflattener.parquet"
     )
     ans_1_data = loadParquet(
-        "/home/nds838/Documents/caa-streamworx/data/parquet_data/agent_flattener_tests"
+        "/home/csl122/Documents/caa-streamworx/data/parquet_data/agent_flattener_tests"
         "/ans_data_1_agentflattener.parquet"
     )
-    result = parser()
+    result = AgentStringFlattener(agentSizeLimit=2, entityName="SM_CLIENTIP", runParser=True
+        ).transform(data_1)
+
     # content test
     assert result.subtract(ans_1_data).count() == 0
 
@@ -25,13 +27,16 @@ def test_1():
 # if parser is not working, run test_2, edit code in AgentStringFlattener to deactivate parser
 def test_2():
     data_2 = loadParquet(
-        "/home/nds838/Documents/caa-streamworx/data/parquet_data/agent_flattener_tests"
+        "/home/csl122/Documents/caa-streamworx/data/parquet_data/agent_flattener_tests"
         "/test_data_2_agentflattener.parquet"
     )
     ans_2_data = loadParquet(
-        "/home/nds838/Documents/caa-streamworx/data/parquet_data/agent_flattener_tests"
+        "/home/csl122/Documents/caa-streamworx/data/parquet_data/agent_flattener_tests"
         "/ans_data_2_agentflattener.parquet"
     )
+
+    result = AgentStringFlattener(agentSizeLimit=2, entityName="SM_CLIENTIP", runParser=True
+                                  ).transform(data_2)
 
     # content test
     assert result.subtract(ans_2_data).count() == 0
