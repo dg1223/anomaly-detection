5a65e9287cb2df5a81c16bf15b32ef0afc4b7b00
ghulamusman <gughulamusman@gmail.com>
2021-08-10 13:14:01 +0000

    making changes to include parquet files

--- tests/test_agentStringFlattener.py
+++ tests/test_agentStringFlattener.py
@@ -4,16 +4,19 @@ from src.caaswx.spark.scripts.loadWriteParquet import loadParquet
 # if parser is now working, run test_1, edit code in AgentStringFlattener to activate parser and uncomment block below
 def test_1():
     data_1 = loadParquet(
-        "/home/csl122/Documents/caa-streamworx/data/parquet_data/agent_flattener_tests"
+        "../data/parquet_data/agent_flattener_tests"
         "/test_data_1_agentflattener.parquet"
     )
     ans_1_data = loadParquet(
-        "/home/csl122/Documents/caa-streamworx/data/parquet_data/agent_flattener_tests"
+        "../data/parquet_data/agent_flattener_tests"
         "/ans_data_1_agentflattener.parquet"
     )
     result = AgentStringFlattener(agentSizeLimit=2, entityName="SM_CLIENTIP", runParser=True
         ).transform(data_1)
-
+    result.show()
+    print(result)
+    ans_1_data.show()
+    print(ans_1_data)
     # content test
     assert result.subtract(ans_1_data).count() == 0
 
@@ -27,15 +30,15 @@ def test_1():
 # if parser is not working, run test_2, edit code in AgentStringFlattener to deactivate parser
 def test_2():
     data_2 = loadParquet(
-        "/home/csl122/Documents/caa-streamworx/data/parquet_data/agent_flattener_tests"
+        "./data/parquet_data/agent_flattener_tests"
         "/test_data_2_agentflattener.parquet"
     )
     ans_2_data = loadParquet(
-        "/home/csl122/Documents/caa-streamworx/data/parquet_data/agent_flattener_tests"
+        "./data/parquet_data/agent_flattener_tests"
         "/ans_data_2_agentflattener.parquet"
     )
 
-    result = AgentStringFlattener(agentSizeLimit=2, entityName="SM_CLIENTIP", runParser=True
+    result = AgentStringFlattener(agentSizeLimit=2, entityName="SM_CLIENTIP", runParser=False
                                   ).transform(data_2)
 
     # content test
@@ -46,3 +49,5 @@ def test_2():
 
     # row test
     assert result.count() == ans_2_data.count()
+
+test_1()
