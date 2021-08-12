c1a8d3f42f5ec8843a6d544c3acab1990e3acdcd
ghulamusman <gughulamusman@gmail.com>
2021-08-12 16:12:09 +0000

    linter fixes

--- src/caaswx/spark/scripts/nullswap.py
+++ src/caaswx/spark/scripts/nullswap.py
@@ -1,10 +1,8 @@
-from pyspark.sql.types import (
-    StructType,
-    ArrayType
-)
+from pyspark.sql.types import StructType, ArrayType
+
 
 def nullSwap(st1, st2):
-    """Function to swap datatype null parameter within a nested dataframe schema """
+    """Function to swap datatype null parameter within a nested dataframe schema"""
     for sf in st1:
         sf.nullable = st2[sf.name].nullable
         if isinstance(sf.dataType, StructType):
--- tests/test_agentStringFlattener.py
+++ tests/test_agentStringFlattener.py
@@ -16,57 +16,72 @@ from pyspark.sql.types import (
 )
 from pyspark.sql.session import SparkSession
 import os
-spark = SparkSession.builder.getOrCreate()
 
 spark = SparkSession.builder.getOrCreate()
+
 
 def test_1():
-    df = spark.read.parquet('./data/parquet_data/agentStringFlattener_tests/df.parquet')
-    result = AgentStringFlattener(agentSizeLimit=2, entityName="SM_CLIENTIP", runParser=True).transform(df)
+    df = spark.read.parquet("./data/parquet_data/agentStringFlattener_tests/df.parquet")
+    result = AgentStringFlattener(
+        agentSizeLimit=2, entityName="SM_CLIENTIP", runParser=True
+    ).transform(df)
 
-    ans_1_data = spark.read.parquet('./data/parquet_data/agentStringFlattener_tests/ans_1_df.parquet')
+    ans_1_data = spark.read.parquet(
+        "./data/parquet_data/agentStringFlattener_tests/ans_1_df.parquet"
+    )
 
-    df2_schema_filePath = "./data/JSON/agent_flattener_tests/ans_data_1_schema_agentflattener.json"
+    df2_schema_filePath = (
+        "./data/JSON/agent_flattener_tests/ans_data_1_schema_agentflattener.json"
+    )
 
     # ans_1_data = spark.read.json(df2_filePath)
     with open(df2_schema_filePath) as json_file:
         ans_1_data_schema = json.load(json_file)
 
-    ans_1_data_schema = pyspark.sql.types.StructType.fromJson(json.loads(ans_1_data_schema))
+    ans_1_data_schema = pyspark.sql.types.StructType.fromJson(
+        json.loads(ans_1_data_schema)
+    )
 
     # content test
-    assert (result.subtract(ans_1_data).count() == 0)
+    assert result.subtract(ans_1_data).count() == 0
 
     # schema test
     nullSwap(ans_1_data.schema, ans_1_data_schema)
-    assert(result.schema == ans_1_data.schema)
+    assert result.schema == ans_1_data.schema
 
     # row test
-    assert (result.count() == ans_1_data.count())
+    assert result.count() == ans_1_data.count()
+
 
 def test_2():
-    df = spark.read.parquet(
-        './data/parquet_data/agentStringFlattener_tests/df.parquet')
-    result = AgentStringFlattener(agentSizeLimit=2, entityName="SM_CLIENTIP", runParser=False).transform(df)
+    df = spark.read.parquet("./data/parquet_data/agentStringFlattener_tests/df.parquet")
+    result = AgentStringFlattener(
+        agentSizeLimit=2, entityName="SM_CLIENTIP", runParser=False
+    ).transform(df)
 
     ans_2_data = spark.read.parquet(
-        './data/parquet_data/agentStringFlattener_tests/ans_2_df.parquet')
+        "./data/parquet_data/agentStringFlattener_tests/ans_2_df.parquet"
+    )
 
-    df4_schema_filePath = "./data/JSON/agent_flattener_tests/ans_data_2_schema_agentflattener.json"
+    df4_schema_filePath = (
+        "./data/JSON/agent_flattener_tests/ans_data_2_schema_agentflattener.json"
+    )
     with open(df4_schema_filePath) as json_file:
         ans_2_data_schema = json.load(json_file)
 
-    ans_2_data_schema = pyspark.sql.types.StructType.fromJson(json.loads(ans_2_data_schema))
+    ans_2_data_schema = pyspark.sql.types.StructType.fromJson(
+        json.loads(ans_2_data_schema)
+    )
 
     # content test
-    assert (result.subtract(ans_2_data).count() == 0)
+    assert result.subtract(ans_2_data).count() == 0
 
     # schema test
     nullSwap(ans_2_data.schema, ans_2_data_schema)
     print(result.schema)
     print(ans_2_data_schema)
     print(ans_2_data.schema)
-    assert (result.schema == ans_2_data.schema)
+    assert result.schema == ans_2_data.schema
 
     # row test
-    assert (result.count() == ans_2_data.count())
+    assert result.count() == ans_2_data.count()
