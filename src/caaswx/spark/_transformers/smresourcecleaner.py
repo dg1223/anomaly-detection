0f4d3896fbc6ac06e273cf77fd1bc56b7a7cada3
Ubuntu <csl122@csl122-vm2.j5bptqmcza4uvpu5i0ekuzskuh.ux.internal.cloudapp.net>
2021-08-13 20:52:39 +0000

    no lint issue when I run black, but...

--- src/caaswx/spark/_transformers/smresourcecleaner.py
+++ src/caaswx/spark/_transformers/smresourcecleaner.py
@@ -5,27 +5,24 @@ from pyspark.sql.types import StringType
 
 
 class SMResourceCleaner(Transformer):
-
     """
-    Consolidates SM_RESOURCE elements to simplify redundant data, based off of the following criteria:
-    1) SAML Requests
-      Suggested Categorization: Strings containing the prefix '/cmsws' and substrings 'redirect' and 'SAML'.
-      Action: Replace with the string '<SAML request>'
-    2) Query strings
-      Suggested Categorization: Strings containing the character '?' after the last occurrence of '/'.
-      Action: Replace everything after the relevant '?' by '*'.
-    3) Other strings
-      Suggested Categorization: Take whatever's left over from the previous two categories that isn't null.
-      Action: Do nothing.
-  
-    Input: The dataframe containing SM_RESOURCE that needs needs to be cleaned.
-    Output: Dataframe appended with cleaned SM_RESOURCE. 
-  
-    Notes: In some entries there may exist some long 
+        Consolidates SM_RESOURCE elements to simplify redundant data, based off of the following criteria:
+        1) SAML Requests
+          Suggested Categorization: Strings containing the prefix '/cmsws' and substrings 'redirect' and 'SAML'.
+          Action: Replace with the string '<SAML request>'
+        2) Query strings
+          Suggested Categorization: Strings containing the character '?' after the last occurrence of '/'.
+          Action: Replace everything after the relevant '?' by '*'.
+        3) Other strings
+          Suggested Categorization: Take whatever's left over from the previous two categories that isn't null.
+          Action: Do nothing.
+
+        Input: The dataframe containing SM_RESOURCE that needs needs to be cleaned.
+        Output: Dataframe appended with cleaned SM_RESOURCE.
+        Notes: In some entries there may exist some long
     """
 
     def _transform(self, dataset):
-
         dataset = dataset.withColumn(
             "Cleaned_SM_RESOURCE",
             regexp_replace(
