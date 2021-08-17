41f7ed6401f8a7248888255c52063866aeae34c0
Ubuntu <vmadmin@chitra.j5bptqmcza4uvpu5i0ekuzskuh.ux.internal.cloudapp.net>
2021-08-17 19:22:03 +0000

    cleaned up the test file a bit

--- tests/test_smResourceCleaner.py
+++ tests/test_smResourceCleaner.py
@@ -5,22 +5,25 @@ import pyspark.sql.types
 from pyspark.sql.session import SparkSession
 from src.caaswx.spark._transformers.smresourcecleaner import SMResourceCleaner
 from src.caaswx.spark.scripts.nullswap import nullSwap
+from src.caaswx.spark.scripts.loadtestdata import load_test_data, load_path
 
 spark = SparkSession.builder.getOrCreate()
 
 
 def test_1():
     local_path = pathlib.Path().resolve()
-    df = spark.read.parquet(
-        str(local_path) + "/data/parquet_data/sm_resource_tests/test_data.parquet"
+    df = load_test_data(
+        "data", "parquet_data", "sm_resource_tests", "test_data.parquet"
     )
+
     result = SMResourceCleaner().transform(df)
 
-    ans_1_data = spark.read.parquet(
-        str(local_path) + "/data/parquet_data/sm_resource_tests/ans_data.parquet"
+    ans_1_data = load_test_data(
+        "data", "parquet_data", "sm_resource_tests", "ans_data.parquet"
     )
-    df2_schema_filePath = (
-        str(local_path) + "/data/JSON/sm_resource_tests/ans_data_schema.json"
+
+    df2_schema_filePath = load_path(
+        "data", "JSON", "sm_resource_tests", "ans_data_schema.json"
     )
 
     # ans_1_data = spark.read.json(df2_filePath)
