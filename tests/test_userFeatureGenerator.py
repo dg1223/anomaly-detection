55bba3366270f562137b6f02c9b2707b695ae338
ghulamusman <gughulamusman@gmail.com>
2021-09-01 15:43:15 +0000

    IP feature generator test suite

---
+++ tests/test_userFeatureGenerator.py
@@ -0,0 +1,31 @@
+import json
+import pathlib
+
+import pyspark.sql.types
+from pyspark.sql.session import SparkSession
+from src.caaswx.spark._transformers.userfeaturegenerator import (
+    UserFeatureGenerator,
+)
+from src.caaswx.spark.scripts.nullswap import nullSwap
+from src.caaswx.spark.scripts.loadtestdata import load_test_data, load_path
+
+spark = SparkSession.builder.getOrCreate()
+
+
+def test_1():
+
+    test_df = spark.read.parquet(
+        "data/parquet_data/user_feature_generator_tests/df.parquet"
+    )
+    ans_1_data = spark.read.parquet(
+        "./data/parquet_data/user_feature_generator_tests/ans_data.parquet"
+    )
+
+    fg = UserFeatureGenerator()
+    result = fg.transform(test_df)
+
+    # content test
+    assert result.subtract(ans_1_data).count() == 0
+
+    # row test
+    assert result.count() == ans_1_data.count()
