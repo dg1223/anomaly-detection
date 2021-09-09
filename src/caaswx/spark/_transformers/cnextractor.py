aa3caec4913e73b3260222f99b1ff54adef27492
AndrewGPeng <andrewpoky@gmail.com>
2021-09-09 09:20:09 -0400

    Update cnextractor.py
    removed redundant code

--- src/caaswx/spark/_transformers/cnextractor.py
+++ src/caaswx/spark/_transformers/cnextractor.py
@@ -1,26 +1,69 @@
-from pyspark.ml import UnaryTransformer
+from src.caaswx.spark._transformers.sparknativetransformer import SparkNativeTransformer
 from pyspark.sql.types import StringType
+import pyspark.sql.functions as F
+from pyspark.sql.functions import when, col, regexp_replace
+from pyspark.ml.param.shared import (
+    TypeConverters,
+    Param,
+    Params,
+    HasInputCol,
+    HasOutputCol,
+)
+from pyspark import keyword_only
 
 
-class CnExtractor(UnaryTransformer):
-    def __init__(self, setInputCol, setOutputCol):
+class CnExtractor(SparkNativeTransformer, HasInputCol, HasOutputCol):
+    """
+    Creates a CN column using the existing SM_USERNAME column
+    by:
+    - Removing the "cn=" all chars before it
+    - Removing the characters after the first comma (including the comma)
+    Input: Dataframe containing an SM_USERNAME that has a CN
+    Output: Same dataframe with a CN column appended
+    Notes:
+    - Assumes the "cn=" and its contents are not at the end of the SM_USERNAME
+    - Reminder that dict must change if SM_USERNAME is no longer used
+    """
+
+    @keyword_only
+    def __init__(self):
+        """
+        init (by default)
+        inputCol: SM_USERNAME
+        outputCol: CN
+        """
         super(CnExtractor, self).__init__()
-        self.setOutputCol(setOutputCol)
-        self.setInputCol(setInputCol)
+        self._setDefault(inputCol="SM_USERNAME", outputCol="CN")
+        kwargs = self._input_kwargs
+        self.set_params(**kwargs)
 
-    def outputDataType(self):
-        return StringType()
+    @keyword_only
+    def set_params(self):
+        """
+        Sets params for the CN extractor
+        """
+        kwargs = self._input_kwargs
+        return self._set(**kwargs)
 
-    def validateInputType(self, inputType) -> None:
-        if inputType != StringType():
-            raise Exception("Invalid inputType")
+    sch_dict = {
+        "SM_USERNAME": ["SM_USERNAME", StringType()],
+    }
 
-    def cleanUsername(self, row: str) -> str:
-        row = row.split(",", 2)[0]
-        if "cn=" in row:
-            return row.split("=")[1]
-        else:
-            return row
+    def _transform(self, dataset):
+        """
+        Transform the new CN column
+        Params:
+        - dataset: dataframe containing SM_USERNAME, to have CN extracted
+        Returns:
+        - dataset with CN appended
+        """
+        dataset = dataset.withColumn(
+            self.getOrDefault("outputCol"),
+            regexp_replace(dataset[self.getOrDefault("inputCol")], r".*(cn=)", ""),
+        )
+        dataset = dataset.withColumn(
+            self.getOrDefault("outputCol"),
+            regexp_replace(dataset[self.getOrDefault("outputCol")], r"(,.*)$", ""),
+        )
 
-    def createTransformFunc(self):
-        return self.cleanUsername
+        return dataset
