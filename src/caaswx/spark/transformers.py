f75e4f7b3333b90102417d0d4e7a477530b66392
ghulamusman <38711855+ghulamusman@users.noreply.github.com>
2021-09-13 14:32:29 -0400

    transformer.py now working
    Added references to all transformers that are accessible to the user. 
    Sparknativetransformer not included.
    Each class was instantiated here to avoid flake8 error.

--- src/caaswx/spark/transformers.py
+++ src/caaswx/spark/transformers.py
@@ -1,5 +1,32 @@
-# from _transformers.UserFeatureGenerator import (
-#     SessionFeatureGenerator,
-#     UserFeatureGenerator,
-#     IPFeatureGenerator,
-# )
+from src.caaswx.spark._transformers.agentstringflattener import (
+    AgentStringFlattener
+)
+from src.caaswx.spark._transformers.cnextractor import (
+    CnExtractor
+)
+from src.caaswx.spark._transformers.ipfeaturegenerator import (
+    IPFeatureGenerator
+)
+from src.caaswx.spark._transformers.resourcesflattener import (
+    ResourcesFlattener
+)
+from src.caaswx.spark._transformers.sessionfeaturegenerator import (
+    SessionFeatureGenerator
+)
+from src.caaswx.spark._transformers.serverfeaturegenerator import (
+    ServerFeatureGenerator
+)
+from src.caaswx.spark._transformers.smresourcecleaner import (
+    SMResourceCleaner
+)
+from src.caaswx.spark._transformers.userfeaturegenerator import (
+    UserFeatureGenerator
+)
+asf = AgentStringFlattener()
+cn = CnExtractor()
+ip = IPFeatureGenerator()
+rs = ResourcesFlattener()
+sesf = SessionFeatureGenerator()
+serf = ServerFeatureGenerator()
+sm = SMResourceCleaner()
+uf = UserFeatureGenerator()
