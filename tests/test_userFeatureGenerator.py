54a90e07d954226326a7eef1b749834ba0e9cbca
Andrew <andrewpoky@gmail.com>
2021-09-13 18:40:27 +0000

    Updated tests too

--- tests/test_userFeatureGenerator.py
+++ tests/test_userFeatureGenerator.py
@@ -1,11 +1,13 @@
 import json
-import pathlib
 
 import pyspark.sql.types
 from pyspark.sql.session import SparkSession
-from src.caaswx.spark._transformers.userfeaturegenerator import UserFeatureGenerator
-from src.caaswx.spark.scripts.nullswap import nullSwap
+
+from src.caaswx.spark._transformers.userfeaturegenerator import (
+    UserFeatureGenerator,
+)
 from src.caaswx.spark.scripts.loadtestdata import load_test_data, load_path
+from src.caaswx.spark.scripts.nullswap import null_swap
 
 spark = SparkSession.builder.getOrCreate()
 
@@ -16,18 +18,21 @@ def test_1():
     )
 
     ans_1_data = load_test_data(
-        "data", "parquet_data", "user_feature_generator_tests", "ans_data.parquet"
+        "data",
+        "parquet_data",
+        "user_feature_generator_tests",
+        "ans_data.parquet",
     )
 
     fg = UserFeatureGenerator()
     result = fg.transform(test_df)
 
-    df2_schema_filePath = load_path(
+    df2_schema_file_path = load_path(
         "data", "JSON", "user_feature_generator_tests", "ans_data_schema.json"
     )
 
     # ans_1_data = spark.read.json(df2_filePath)
-    with open(df2_schema_filePath) as json_file:
+    with open(df2_schema_file_path) as json_file:
         ans_1_data_schema = json.load(json_file)
 
     ans_1_data_schema = pyspark.sql.types.StructType.fromJson(
@@ -41,5 +46,5 @@ def test_1():
     assert result.count() == ans_1_data.count()
 
     # schema test
-    nullSwap(ans_1_data.schema, ans_1_data_schema)
+    null_swap(ans_1_data.schema, ans_1_data_schema)
     assert result.schema == ans_1_data.schema
