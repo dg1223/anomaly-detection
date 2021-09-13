82bd13ac067f05e3c9216a48471e5e5c4a785ab3
Andrew <andrewpoky@gmail.com>
2021-09-13 18:50:40 +0000

    Fixed file and test

--- src/caaswx/spark/_transformers/sessionfeaturegenerator.py
+++ src/caaswx/spark/_transformers/sessionfeaturegenerator.py
@@ -2,54 +2,73 @@
 A module to generate features regarding to session feature
 Input: A dataframe with a CN row.
 Output: A dataframe with the following features extracted:
-
-SESSION_APPS	            A distinct list of root nodes from each record in SM_RESOURCE during time window. 
-COUNT_UNIQUE_APPS	        A count of distinct root nodes from each record in SM_RESOURCE during time window.
-SESSION_USER    	        A distinct list of CNs in CN during time window. 
-COUNT_ADMIN_LOGIN      	    Count of Admin Login events during the time window, defined by sm_eventid = 7.
-COUNT_ADMIN_LOGOUT  	    Count of Admin Logout events during the time window, defined by sm_eventid = 8.
-COUNT_ADMIN_REJECT  	    Count of Admin Reject events during the time window, defined by sm_eventid = 2.
-COUNT_FAILED        	    Count of all Reject events during the time window, defined by sm_eventid = 2, 6 and 9.
-COUNT_VISIT         	    Count of Visit events during the time window, defined by sm_eventid = 13.
-COUNT_GET	                Count of all GET HTTP actions in SM_ACTION during the time window.
-COUNT_POST	                Count of all POST HTTP actions in SM_ACTION during the time window.
-COUNT_HTTP_METHODS	        Count of all GET and POST HTTP actions in SM_ACTION  during the time window.
+SESSION_APPS	            A distinct list of root nodes from each record in
+                                SM_RESOURCE during time window.
+COUNT_UNIQUE_APPS	        A count of distinct root nodes from each record
+                                in SM_RESOURCE during time window.
+SESSION_USER    	        A distinct list of CNs in CN during time window.
+COUNT_ADMIN_LOGIN      	    Count of Admin Login events during the time
+                                window, defined by sm_eventid = 7.
+COUNT_ADMIN_LOGOUT  	    Count of Admin Logout events during the time
+                                window, defined by sm_eventid = 8.
+COUNT_ADMIN_REJECT  	    Count of Admin Reject events during the time
+                                window, defined by sm_eventid = 2.
+COUNT_FAILED        	    Count of all Reject events during the time
+                                window, defined by sm_eventid = 2, 6 and 9.
+COUNT_VISIT         	    Count of Visit events during the time window,
+                                defined by sm_eventid = 13.
+COUNT_GET	                Count of all GET HTTP actions in SM_ACTION
+                                during the time window.
+COUNT_POST	                Count of all POST HTTP actions in SM_ACTION
+                                during the time window.
+COUNT_HTTP_METHODS	        Count of all GET and POST HTTP actions in
+                                SM_ACTION  during the time window.
 COUNT_RECORDS	            Counts number of CRA_SEQs (dataset primary key)
-COUNT_UNIQUE_ACTIONS   	    Count of distinct HTTP Actions in SM_ACTION during the time window.
-COUNT_UNIQUE_EVENTS	        Count of distinct EventIDs in SM_EVENTID  during the time window.
-COUNT_UNIQUE_USERNAME	    Count of distinct CNs in CN during the time window.
-COUNT_UNIQUE_RESOURCES	    Count of distinct Resource Strings in SM_RESOURCE during the time window.
-COUNT_UNIQUE_REP	        A count of Entries containing “rep” followed by a string ending in “/” in SM_RESOURCE during time window.
-SESSION_SM_ACTION	        A distinct list of HTTP Actions in SM_ACTION during time window. 
-SESSION_RESOURCE	        A distinct list of Resource Strings in SM_RESOURCE during time window.
-SESSION_REP_APP	            A distinct list of Entries containing “rep” followed by a string ending in “/” in SM_RESOURCE during time window.
-SESSSION_FIRST_TIME_SEEN	Minimum time at which a record was logged during the time window.
-SESSSION_LAST_TIME_SEEN	    Maximum time at which a record was logged during the time window.
-SDV_BT_RECORDS	            Standard deviation of timestamp deltas during the time window. 
-
-
+COUNT_UNIQUE_ACTIONS   	    Count of distinct HTTP Actions in SM_ACTION
+                                during the time window.
+COUNT_UNIQUE_EVENTS	        Count of distinct EventIDs in SM_EVENTID during
+                                the time window.
+COUNT_UNIQUE_USERNAME	    Count of distinct CNs in CN during the time
+                                window.
+COUNT_UNIQUE_RESOURCES	    Count of distinct Resource Strings in
+                                SM_RESOURCE during the time window.
+COUNT_UNIQUE_REP            A count of Entries containing “rep”
+                                followed by a string ending in “/”
+                                in SM_RESOURCE during time window.
+SESSION_SM_ACTION	        A distinct list of HTTP Actions in
+                                SM_ACTION during time window.
+SESSION_RESOURCE	        A distinct list of Resource Strings in
+                                SM_RESOURCE during time window.
+SESSION_REP_APP	            A distinct list of Entries containing
+                                “rep” followed by a string ending
+                                in “/” in SM_RESOURCE during time
+                                window.
+SESSION_FIRST_TIME_SEEN     Minimum time at which a record was
+                                logged during the time window.
+SESSION_LAST_TIME_SEEN	    Maximum time at which a record was logged
+                                during the time window.
+SDV_BT_RECORDS	            Standard deviation of timestamp deltas
+                                during the time window.
 """
 
-import pyspark.sql.functions as F
-
+import pyspark.sql.functions as f
 # Import Essential packages
 from pyspark import keyword_only
-from pyspark.ml import Transformer
 from pyspark.ml.param.shared import TypeConverters, Param, Params
 from pyspark.sql.functions import col, when, lag, isnull
 from pyspark.sql.functions import regexp_extract
 from pyspark.sql.functions import window
 from pyspark.sql.types import (
-    StructType,
-    ArrayType,
     LongType,
     StringType,
-    StructField,
     TimestampType,
     DoubleType,
 )
 from pyspark.sql.window import Window
-from src.caaswx.spark._transformers.sparknativetransformer import SparkNativeTransformer
+
+from src.caaswx.spark._transformers.sparknativetransformer import (
+    SparkNativeTransformer,
+)
 
 
 class SessionFeatureGenerator(SparkNativeTransformer):
@@ -59,7 +78,7 @@ class SessionFeatureGenerator(SparkNativeTransformer):
 
     window_length = Param(
         Params._dummy(),
-        "windowLength",
+        "window_length",
         "Length of the sliding window used for entity resolution. "
         + "Given as an integer in seconds.",
         typeConverter=TypeConverters.toInt,
@@ -67,7 +86,7 @@ class SessionFeatureGenerator(SparkNativeTransformer):
 
     window_step = Param(
         Params._dummy(),
-        "windowStep",
+        "window_step",
         "Length of the sliding window step-size used for entity resolution. "
         + "Given as an integer in seconds.",
         typeConverter=TypeConverters.toInt,
@@ -86,9 +105,9 @@ class SessionFeatureGenerator(SparkNativeTransformer):
     @keyword_only
     def set_params(self):
         """
-        set_params(self, \\*, threshold=0.0, inputCol=None, outputCol=None, thresholds=None, \
-                  inputCols=None, outputCols=None)
-        Sets params for this SessionFeatureGenerator.
+        set_params(self, \\*, threshold=0.0, inputCol=None, outputCol=None,
+        thresholds=None, inputCols=None, outputCols=None) Sets params for
+        this SessionFeatureGenerator.
         """
         kwargs = self._input_kwargs
         return self._set(**kwargs)
@@ -116,7 +135,6 @@ class SessionFeatureGenerator(SparkNativeTransformer):
     }
 
     def _transform(self, dataset):
-
         ts_window = Window.partitionBy("SM_SESSIONID").orderBy("SM_TIMESTAMP")
 
         dataset = dataset.withColumn(
@@ -147,26 +165,38 @@ class SessionFeatureGenerator(SparkNativeTransformer):
                 str(self.getOrDefault("window_step")) + " seconds",
             ),
         ).agg(
-            F.array_remove(
-                F.array_distinct(
-                    F.collect_list(regexp_extract("SM_RESOURCE", r"/(.*?)/", 0))
+            f.array_remove(
+                f.array_distinct(
+                    f.collect_list(
+                        regexp_extract("SM_RESOURCE", r"/(.*?)/", 0)
+                    )
                 ),
                 "",
             ).alias("SESSION_APPS"),
-            F.size(
-                F.array_remove(
-                    F.array_distinct(
-                        F.collect_list(regexp_extract("SM_RESOURCE", r"/(.*?)/", 0))
+            f.size(
+                f.array_remove(
+                    f.array_distinct(
+                        f.collect_list(
+                            regexp_extract("SM_RESOURCE", r"/(.*?)/", 0)
+                        )
                     ),
                     "",
                 )
             ).alias("COUNT_UNIQUE_APPS"),
-            F.array_distinct(F.collect_list(col("CN"))).alias("SESSION_USER"),
-            F.count(when(col("SM_EVENTID") == 7, True)).alias("COUNT_ADMIN_LOGIN"),
-            F.count(when(col("SM_EVENTID") == 8, True)).alias("COUNT_ADMIN_LOGOUT"),
-            F.count(when(col("SM_EVENTID") == 3, True)).alias("COUNT_AUTH_ATTEMPT"),
-            F.count(when(col("SM_EVENTID") == 2, True)).alias("COUNT_AUTH_REJECT"),
-            F.count(
+            f.array_distinct(f.collect_list(col("CN"))).alias("SESSION_USER"),
+            f.count(when(col("SM_EVENTID") == 7, True)).alias(
+                "COUNT_ADMIN_LOGIN"
+            ),
+            f.count(when(col("SM_EVENTID") == 8, True)).alias(
+                "COUNT_ADMIN_LOGOUT"
+            ),
+            f.count(when(col("SM_EVENTID") == 3, True)).alias(
+                "COUNT_AUTH_ATTEMPT"
+            ),
+            f.count(when(col("SM_EVENTID") == 2, True)).alias(
+                "COUNT_AUTH_REJECT"
+            ),
+            f.count(
                 when(
                     (col("SM_EVENTID") == 2)
                     | (col("SM_EVENTID") == 6)
@@ -174,26 +204,32 @@ class SessionFeatureGenerator(SparkNativeTransformer):
                     True,
                 )
             ).alias("COUNT_FAILED"),
-            F.count(when(col("SM_EVENTID") == 13, True)).alias("COUNT_VISIT"),
-            F.count(when(col("SM_ACTION").contains("GET"), True)).alias("COUNT_GET"),
-            F.count(when(col("SM_ACTION").contains("POST"), True)).alias("COUNT_POST"),
-            F.count(
+            f.count(when(col("SM_EVENTID") == 13, True)).alias("COUNT_VISIT"),
+            f.count(when(col("SM_ACTION").contains("GET"), True)).alias(
+                "COUNT_GET"
+            ),
+            f.count(when(col("SM_ACTION").contains("POST"), True)).alias(
+                "COUNT_POST"
+            ),
+            f.count(
                 when(
                     (col("SM_ACTION").contains("GET"))
                     | (col("SM_ACTION").contains("POST")),
                     True,
                 )
             ).alias("COUNT_HTTP_METHODS"),
-            F.count(col("CRA_SEQ")).alias("COUNT_RECORDS"),
-            F.countDistinct(col("SM_ACTION")).alias("COUNT_UNIQUE_ACTIONS"),
-            F.countDistinct(col("SM_EVENTID")).alias("COUNT_UNIQUE_EVENTS"),
-            F.countDistinct(col("SM_CLIENTIP")).alias("COUNT_UNIQUE_IPS"),
-            F.countDistinct(col("SM_RESOURCE")).alias("COUNT_UNIQUE_RESOURCES"),
+            f.count(col("CRA_SEQ")).alias("COUNT_RECORDS"),
+            f.countDistinct(col("SM_ACTION")).alias("COUNT_UNIQUE_ACTIONS"),
+            f.countDistinct(col("SM_EVENTID")).alias("COUNT_UNIQUE_EVENTS"),
+            f.countDistinct(col("SM_CLIENTIP")).alias("COUNT_UNIQUE_IPS"),
+            f.countDistinct(col("SM_RESOURCE")).alias(
+                "COUNT_UNIQUE_RESOURCES"
+            ),
             (
-                F.size(
-                    F.array_remove(
-                        F.array_distinct(
-                            F.collect_list(
+                f.size(
+                    f.array_remove(
+                        f.array_distinct(
+                            f.collect_list(
                                 regexp_extract("SM_RESOURCE", r"(rep.*?)/", 0)
                             )
                         ),
@@ -201,21 +237,23 @@ class SessionFeatureGenerator(SparkNativeTransformer):
                     )
                 ).alias("COUNT_UNIQUE_REP")
             ),
-            F.array_distinct(F.collect_list(col("SM_ACTION"))).alias(
+            f.array_distinct(f.collect_list(col("SM_ACTION"))).alias(
                 "SESSION_SM_ACTION"
             ),
-            F.array_distinct(F.collect_list(col("SM_RESOURCE"))).alias(
+            f.array_distinct(f.collect_list(col("SM_RESOURCE"))).alias(
                 "SESSION_RESOURCE"
             ),
-            F.array_remove(
-                F.array_distinct(
-                    F.collect_list(regexp_extract("SM_RESOURCE", r"(rep.*?)/", 0))
+            f.array_remove(
+                f.array_distinct(
+                    f.collect_list(
+                        regexp_extract("SM_RESOURCE", r"(rep.*?)/", 0)
+                    )
                 ),
                 "",
             ).alias("SESSION_REP_APP"),
-            F.min(col("SM_TIMESTAMP")).alias("SESSSION_FIRST_TIME_SEEN"),
-            F.max(col("SM_TIMESTAMP")).alias("SESSSION_LAST_TIME_SEEN"),
-            F.round(F.stddev("SM_CONSECUTIVE_TIME_DIFFERENCE"), 15).alias(
+            f.min(col("SM_TIMESTAMP")).alias("SESSSION_FIRST_TIME_SEEN"),
+            f.max(col("SM_TIMESTAMP")).alias("SESSSION_LAST_TIME_SEEN"),
+            f.round(f.stddev("SM_CONSECUTIVE_TIME_DIFFERENCE"), 15).alias(
                 "SDV_BT_RECORDS"
             ),
         )
