e06919194a8d6a0ed0b7530f26a677f320cb21ab
ghulamusman <38711855+ghulamusman@users.noreply.github.com>
2021-09-13 15:44:04 -0400

    ignore certain errors

--- src/caaswx/spark/transformers.py
+++ src/caaswx/spark/transformers.py
@@ -1,34 +1,24 @@
-"""
-from src.caaswx.spark._transformers.agentstringflattener import (
+from src.caaswx.spark._transformers.agentstringflattener import (  # noqa: F401
     AgentStringFlattener
 )
-from src.caaswx.spark._transformers.cnextractor import (
+from src.caaswx.spark._transformers.cnextractor import (  # noqa: F401
     CnExtractor
 )
-from src.caaswx.spark._transformers.ipfeaturegenerator import (
+from src.caaswx.spark._transformers.ipfeaturegenerator import (  # noqa: F401
     IPFeatureGenerator
 )
-from src.caaswx.spark._transformers.resourcesflattener import (
+from src.caaswx.spark._transformers.resourcesflattener import (  # noqa: F401
     ResourcesFlattener
 )
-from src.caaswx.spark._transformers.sessionfeaturegenerator import (
+from src.caaswx.spark._transformers.sessionfeaturegenerator import (  # noqa: F401,E501
     SessionFeatureGenerator
 )
-from src.caaswx.spark._transformers.serverfeaturegenerator import (
+from src.caaswx.spark._transformers.serverfeaturegenerator import (  # noqa: F401,E501
     ServerFeatureGenerator
 )
-from src.caaswx.spark._transformers.smresourcecleaner import (
+from src.caaswx.spark._transformers.smresourcecleaner import (  # noqa: F401
     SMResourceCleaner
 )
-from src.caaswx.spark._transformers.userfeaturegenerator import (
+from src.caaswx.spark._transformers.userfeaturegenerator import (  # noqa: F401
     UserFeatureGenerator
 )
-asf = AgentStringFlattener()
-cn = CnExtractor()
-ip = IPFeatureGenerator()
-rs = ResourcesFlattener()
-sesf = SessionFeatureGenerator()
-serf = ServerFeatureGenerator()
-sm = SMResourceCleaner()
-uf = UserFeatureGenerator()
-"""
