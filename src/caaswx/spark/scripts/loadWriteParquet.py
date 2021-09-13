7d3a7974a5ae7e6e46113d725078a1745db17c82
sagar <sagarmoghe5@gmail.com>
2021-09-13 14:47:32 -0300

    Fixed loadWriteParquet.py

--- src/caaswx/spark/scripts/loadWriteParquet.py
+++ src/caaswx/spark/scripts/loadWriteParquet.py
@@ -5,13 +5,13 @@ from pyspark.sql.session import SparkSession
 spark = SparkSession.builder.getOrCreate()
 
 
-def writeParquet(filePath: str, schema, data):
+def write_parquet(filePath: str, schema, data):
     dataFrame = spark.createDataFrame(data, schema=schema)
     dataFrame.write.parquet(filePath)
     return dataFrame
 
 
-def loadParquet(*argv: str):
+def load_parquet(*argv: str):
     cwd = os.getcwd()
     path = os.path.join(cwd, *argv)
     return spark.read.parquet(path)
