9bf0c640f317bac1a196068f30a072ba3772a778
ghulamusman <gughulamusman@gmail.com>
2021-09-20 19:51:06 +0000

    path reorg

--- tests/test_cnExtractor.py
+++ tests/test_cnExtractor.py
@@ -1,18 +1,19 @@
 from src.caaswx.spark._transformers.cnextractor import CnExtractor
-from pyspark.sql.session import SparkSession
+from src.caaswx.spark.scripts.loadWriteParquet import load_parquet
 
-spark = SparkSession.builder.getOrCreate()
 
 def test_cnextractor():
     obj = CnExtractor()
-    test_df = spark.read.parquet(
-        "./data/parquet_data/cn_extractor_tests/SM_USERNAME_2_examples.parquet"
+    test_df = load_parquet(
+        "data", "parquet_data", "cn_extractor_tests",
+        "SM_USERNAME_2_examples.parquet"
     )
-
-    answer_df = spark.read.parquet(
-        "./data/parquet_data/cn_extractor_tests/expected_SM_USERNAME_2_examples.parquet"
+    answer_df = load_parquet(
+        "data",
+        "parquet_data",
+        "cn_extractor_tests",
+        "expected_SM_USERNAME_2_examples.parquet",
     )
-
     test_df.show()
     answer_df.show()
     result_df = obj.transform(test_df)
