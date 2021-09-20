ab04dadb7922fe4d4220f2921656100d28ec2d0d
ghulamusman <gughulamusman@gmail.com>
2021-09-20 19:40:06 +0000

    fixed path issues

--- tests/test_cnExtractor.py
+++ tests/test_cnExtractor.py
@@ -1,19 +1,18 @@
 from src.caaswx.spark._transformers.cnextractor import CnExtractor
-from src.caaswx.spark.scripts.loadWriteParquet import load_parquet
+from pyspark.sql.session import SparkSession
 
+spark = SparkSession.builder.getOrCreate()
 
 def test_cnextractor():
     obj = CnExtractor()
-    test_df = load_parquet(
-        "data", "parquet_data", "cn_extractor_tests",
-        "SM_USERNAME_2_examples.parquet "
+    test_df = spark.read.parquet(
+        "./data/parquet_data/cn_extractor_tests/SM_USERNAME_2_examples.parquet"
     )
-    answer_df = load_parquet(
-        "data",
-        "parquet_data",
-        "cn_extractor_tests",
-        "expected_SM_USERNAME_2_examples.parquet",
+
+    answer_df = spark.read.parquet(
+        "./data/parquet_data/cn_extractor_tests/expected_SM_USERNAME_2_examples.parquet"
     )
+
     test_df.show()
     answer_df.show()
     result_df = obj.transform(test_df)
