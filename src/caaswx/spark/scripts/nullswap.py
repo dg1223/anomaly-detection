47e7fdb6d24cbfb4c2ea4ad30e6040a9b5a05bec
Andrew <andrewpoky@gmail.com>
2021-09-20 21:08:08 +0000

    nullswap black update

--- src/caaswx/spark/scripts/nullswap.py
+++ src/caaswx/spark/scripts/nullswap.py
@@ -3,7 +3,7 @@ from pyspark.sql.types import StructType, ArrayType
 
 def null_swap(st1, st2):
     """Function to swap datatype null parameter within a nested dataframe
-    schema """
+    schema"""
     for sf in st1:
         sf.nullable = st2[sf.name].nullable
         if isinstance(sf.dataType, StructType):
