8df49bedc601af12e34243958c67ba3475cec4f7
Andrew <andrewpoky@gmail.com>
2021-09-23 13:59:25 +0000

    fixed eventid to int

--- src/caaswx/spark/_transformers/serverfeaturegenerator.py
+++ src/caaswx/spark/_transformers/serverfeaturegenerator.py
@@ -8,9 +8,8 @@ from pyspark.sql.functions import (
 )
 from pyspark.sql.functions import window
 from pyspark.sql.types import (
-    LongType,
     StringType,
-    TimestampType,
+    TimestampType, IntegerType,
 )
 from pyspark.sql.window import Window
 
@@ -224,7 +223,7 @@ class ServerFeatureGenerator(SparkNativeTransformer):
 
     sch_dict = {
         "SM_TIMESTAMP": ["SM_TIMESTAMP", TimestampType()],
-        "SM_EVENTID": ["SM_EVENTID", LongType()],
+        "SM_EVENTID": ["SM_EVENTID", IntegerType()],
         "SM_RESOURCE": ["SM_RESOURCE", StringType()],
         "SM_CLIENTIP": ["SM_CLIENTIP", StringType()],
     }
