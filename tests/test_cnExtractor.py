d5ace4cc4b568b4e1649ca5da8dc738677918506
ghulamusman <gughulamusman@gmail.com>
2021-10-13 12:32:18 +0000

    empty data

--- tests/test_cnExtractor.py
+++ tests/test_cnExtractor.py
@@ -16,10 +16,20 @@ def test_cnextractor():
         "cn_extractor_tests",
         "expected_SM_USERNAME_2_examples.parquet",
     )
-    test_df.show()
-    answer_df.show()
     result_df = obj.transform(test_df)
     sub1 = result_df.subtract(answer_df)
     sub2 = answer_df.subtract(result_df)
 
     assert bool(sub1.head(1)) is bool(sub2.head(1)) is False
+
+def test_empty_data():
+    obj = CnExtractor()
+    test_df = load_parquet(
+        "data",
+        "parquet_data",
+        "cn_extractor_tests",
+        "data_empty_df.parquet",
+    )
+    result_df = obj.transform(test_df)
+
+    assert result_df.count() == 0
