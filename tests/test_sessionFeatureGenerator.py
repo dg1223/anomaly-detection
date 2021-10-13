7be26f8b2fa4f1b809947d10ce7645b7b0128715
ghulamusman <gughulamusman@gmail.com>
2021-10-13 14:54:29 +0000

    separate tests

--- tests/test_sessionFeatureGenerator.py
+++ tests/test_sessionFeatureGenerator.py
@@ -9,8 +9,7 @@ from src.caaswx.spark.scripts.loadtestdata import load_test_data, load_path
 
 spark = SparkSession.builder.getOrCreate()
 
-
-def test_1():
+def test_content():
     test_df = load_test_data(
         "data",
         "parquet_data",
@@ -46,9 +45,80 @@ def test_1():
     # content test
     assert result.subtract(ans_1_data).count() == 0
 
-    # row test
-    assert result.count() == ans_1_data.count()
+
+def test_schema():
+    test_df = load_test_data(
+        "data",
+        "parquet_data",
+        "session_feature_generator_tests",
+        "data_df.parquet",
+    )
+
+    ans_1_data = load_test_data(
+        "data",
+        "parquet_data",
+        "session_feature_generator_tests",
+        "ans_data.parquet",
+    )
+
+    fg = SessionFeatureGenerator()
+    result = fg.transform(test_df)
+
+    df2_schema_filepath = load_path(
+        "data",
+        "JSON",
+        "session_feature_generator_tests",
+        "ans_data_schema.json",
+    )
+
+    # ans_1_data = spark.read.json(df2_filePath)
+    with open(df2_schema_filepath) as json_file:
+        ans_1_data_schema = json.load(json_file)
+
+    ans_1_data_schema = pyspark.sql.types.StructType.fromJson(
+        json.loads(ans_1_data_schema)
+    )
 
     # schema test
     null_swap(ans_1_data.schema, ans_1_data_schema)
     assert result.schema == ans_1_data.schema
+
+
+def test_num_rows():
+    test_df = load_test_data(
+        "data",
+        "parquet_data",
+        "session_feature_generator_tests",
+        "data_df.parquet",
+    )
+
+    ans_1_data = load_test_data(
+        "data",
+        "parquet_data",
+        "session_feature_generator_tests",
+        "ans_data.parquet",
+    )
+
+    fg = SessionFeatureGenerator()
+    result = fg.transform(test_df)
+
+    df2_schema_filepath = load_path(
+        "data",
+        "JSON",
+        "session_feature_generator_tests",
+        "ans_data_schema.json",
+    )
+
+    # ans_1_data = spark.read.json(df2_filePath)
+    with open(df2_schema_filepath) as json_file:
+        ans_1_data_schema = json.load(json_file)
+
+    ans_1_data_schema = pyspark.sql.types.StructType.fromJson(
+        json.loads(ans_1_data_schema)
+    )
+
+    # row test
+    assert result.count() == ans_1_data.count()
+
+
+
