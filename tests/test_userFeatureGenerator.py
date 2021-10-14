0b5d449644783b042900689866eaccd2fde4c00c
ghulamusman <gughulamusman@gmail.com>
2021-10-14 18:45:03 +0000

    added test documentation and removed redundant code

--- tests/test_userFeatureGenerator.py
+++ tests/test_userFeatureGenerator.py
@@ -12,6 +12,10 @@ spark = SparkSession.builder.getOrCreate()
 
 
 def test_num_rows():
+    """
+    Tests if number of rows are the same between the results and expected
+    result.
+    """
     test_df = load_test_data(
         "data", "parquet_data", "user_feature_generator_tests", "data.parquet"
     )
@@ -25,24 +29,16 @@ def test_num_rows():
 
     fg = UserFeatureGenerator()
     result = fg.transform(test_df)
-
-    df2_schema_file_path = load_path(
-        "data", "JSON", "user_feature_generator_tests", "ans_data_schema.json"
-    )
-
-    # ans_1_data = spark.read.json(df2_filePath)
-    with open(df2_schema_file_path) as json_file:
-        ans_1_data_schema = json.load(json_file)
-
-    ans_1_data_schema = pyspark.sql.types.StructType.fromJson(
-        json.loads(ans_1_data_schema)
-    )
 
     # row test
     assert result.count() == ans_1_data.count()
 
 
 def test_content():
+    """
+    Tests if the data in the results is the same as the data in the
+    expected result.
+    """
     test_df = load_test_data(
         "data", "parquet_data", "user_feature_generator_tests", "data.parquet"
     )
@@ -57,23 +53,15 @@ def test_content():
     fg = UserFeatureGenerator()
     result = fg.transform(test_df)
 
-    df2_schema_file_path = load_path(
-        "data", "JSON", "user_feature_generator_tests", "ans_data_schema.json"
-    )
-
-    # ans_1_data = spark.read.json(df2_filePath)
-    with open(df2_schema_file_path) as json_file:
-        ans_1_data_schema = json.load(json_file)
-
-    ans_1_data_schema = pyspark.sql.types.StructType.fromJson(
-        json.loads(ans_1_data_schema)
-    )
-
     # content test
     assert result.subtract(ans_1_data).count() == 0
 
 
 def test_schema():
+    """
+    Tests if the schemas are the same between the result and the expected
+    result.
+    """
     test_df = load_test_data(
         "data", "parquet_data", "user_feature_generator_tests", "data.parquet"
     )
@@ -106,6 +94,9 @@ def test_schema():
 
 
 def test_empty_data():
+    """
+    Tests transformer behaviour with an empty dataset.
+    """
     test_df = load_test_data(
         "data",
         "parquet_data",
