f1b233dcc25d10e08f8020328fce6c4b25259e2f
ghulamusman <gughulamusman@gmail.com>
2021-10-25 19:50:27 +0000

    test_featureGeneration.py linter fix

--- tests/test_featureGeneration.py
+++ tests/test_featureGeneration.py
@@ -4,31 +4,32 @@ import src.caaswx.spark.transformers as transform
 spark = SparkSession.builder.getOrCreate()
 
 """
-This set of tests are intended to test the intercompatibility of the separate 
+This set of tests are intended to test the intercompatibility of the separate
 parts of the pipeline.
- 
+
 test_CN_RC_UserFG tests:
  CnExtractor -> SMResourceCleaner -> UserFeatureGenerator
+
 test ASF_RF_SessionFG will test:
  AgentStringFlattner -> ResourceFlattener -> ServerFeatureGenertor
-    ASF and RF currently return new dataframes with just the respective new 
+    ASF and RF currently return new dataframes with just the respective new
     columns, therefore will not work.
 """
 
 
-def test_CN_RC_UserFG():
+def test_cn_rc_userfg():
     """
     Tests if the CnExtractor, SMResouceCleaner and UserFeatureGenertors
     work together with a sample of raw_logs.
     """
 
     df = spark.read.parquet("./data/parquet_data/pipeline_tests/data.parquet")
-    resultCnExtractor = transform.CnExtractor().transform(df)
-    resultSMResourceCleaner = transform.SMResourceCleaner().transform(
-        resultCnExtractor
+    result_cnextractor = transform.CnExtractor().transform(df)
+    result_smresourcecleaner = transform.SMResourceCleaner().transform(
+        result_cnextractor
     )
-    resultFG = transform.UserFeatureGenerator().transform(
-        resultSMResourceCleaner
+    result_fg = transform.UserFeatureGenerator().transform(
+        result_smresourcecleaner
     )
 
-    assert resultFG.count() == 1
+    assert result_fg.count() == 1
