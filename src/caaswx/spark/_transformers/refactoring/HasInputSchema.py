2bf52fb7ed8107968288641aada15edc720c6ea4
Ketan Bhalerao <ketan.bhalerao1450@gmail.com>
2021-11-08 16:38:35 +0000

    Added docstrings to the class

--- src/caaswx/spark/_transformers/refactoring/HasInputSchema.py
+++ src/caaswx/spark/_transformers/refactoring/HasInputSchema.py
@@ -14,6 +14,9 @@ class HasInputSchema(Transformer):
 
     @keyword_only
     def __init__(self, *, input_schema=None):
+        """
+        Constructor accepting the input schema to be verified
+        """
         super(HasInputSchema, self).__init__()
         self._setDefault(input_schema=None)
         kwargs = self._input_kwargs
@@ -21,18 +24,31 @@ class HasInputSchema(Transformer):
 
     @keyword_only
     def setparams(self, *, input_schema=None):
+        """
+        setparams(self, *, input_schema=None)
+        """
         kwargs = self._input_kwargs
         return self._set(**kwargs)
 
     def schema_is_admissable(
         self, schema: pyspark.sql.types.StructType, compare_nulls=False
     ):
+        """
+        Method for verifying if the schema specified in
+        the arugment is a subset of self's schema
+        """
         return schema_is_subset(
             self.input_schema, schema, compare_nulls=compare_nulls
         )
 
     def set_input_schema(self, schema: pyspark.sql.types.StructType):
+        """
+        setter method for the class
+        """
         self.input_schema = schema
 
     def get_input_schema(self):
+        """
+        getter method for the class
+        """
         return self.getOrDefault("input_schema")
