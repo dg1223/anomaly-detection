4a7e205b6f2be35d1962ce83833e8b6c879741f4
sagarmoghe5 <16164018+sagarmoghe5@users.noreply.github.com>
2021-12-13 12:53:02 -0400

    created test_transformer

--- tests/test_transformers.py
+++ tests/test_transformers.py
@@ -3,6 +3,3 @@ import pyspark.sql.types
 from pyspark.sql.session import SparkSession
 from src.caaswx.spark.utilities.transformers import (IPFeatureGenerator,)
 from src.caaswx.spark.utilities.loadtestdata import load_test_data, load_path
-
-
-print("sagar")
