d9d0ce3c80bf126d2267b13273cc09ad7bec6747
sagarmoghe5 <16164018+sagarmoghe5@users.noreply.github.com>
2021-12-13 12:54:11 -0400

    created test_transformer

--- tests/test_transformers.py
+++ tests/test_transformers.py
@@ -1,5 +1,5 @@
 import json
 import pyspark.sql.types
 from pyspark.sql.session import SparkSession
-from src.caaswx.spark.utilities.transformers import (IPFeatureGenerator,)
+from src.caaswx.spark.transformers import (IPFeatureGenerator,)
 from src.caaswx.spark.utilities.loadtestdata import load_test_data, load_path
