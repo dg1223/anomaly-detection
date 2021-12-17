c8e0111ef39cc973a558a36bd99c99396272e636
Andrew <andrewpoky@gmail.com>
2021-12-17 14:37:08 +0000

    fixed DistinctCounter test testingBase

--- tests/test_baseFeatures.py
+++ tests/test_baseFeatures.py
@@ -58,6 +58,42 @@ class TestCounterFeature(CounterFeature, HasTypedInputCol):
         return dataset
 
 
+class TestDistinctCounterFeature(DistinctCounterFeature, HasTypedInputCol):
+    """
+    Feature to test basic DistinctCounterFeature functionality.
+    (Original: CountUniqueEvents)
+    """
+
+    def __init__(self, inputCol="SM_EVENTID", outputCol="COUNT_UNIQUE_EVENTS"):
+        super(TestDistinctCounterFeature, self).__init__(outputCol)
+        self._setDefault(
+            inputCol="SM_EVENTID", outputCol="COUNT_UNIQUE_EVENTS"
+        )
+        self._set(inputCol="SM_EVENTID", inputColType=IntegerType())
+        schema = StructType(
+            [
+                StructField(
+                    self.getOrDefault("inputCol"),
+                    self.getOrDefault("inputColType"),
+                )
+            ]
+        )
+        self.set_input_schema(schema)
+
+    def count_clause(self):
+        """
+        :return: Returns the column SM_EVENTID
+        :rtype: pyspark.sql.Column
+        """
+        return col(self.getOrDefault("inputCol"))
+
+    def pre_op(self, dataset):
+        return dataset
+
+    def post_op(self, dataset):
+        return dataset
+
+
 class TestArrayDistinctFeature(ArrayDistinctFeature, HasTypedInputCol):
     """
     Feature to test basic ArrayDistinctFeature functionality.
@@ -164,35 +200,6 @@ class TestSizeArrayRemoveFeature(SizeArrayRemoveFeature, HasTypedInputCol):
         return dataset
 
 
-class TestDistinctCounterFeature(DistinctCounterFeature, HasTypedInputCol):
-    """
-    Feature to test basic DistinctCounterFeature functionality.
-    """
-
-    def __init__(self, inputCol="SM_EVENTID", outputCol="COUNT_AUTH_ACCEPT"):
-        super(TestDistinctCounterFeature, self).__init__(outputCol)
-        self._setDefault(inputCol="SM_EVENTID", outputCol="COUNT_AUTH_ACCEPT")
-        self._set(inputCol="SM_EVENTID", inputColType=IntegerType())
-        schema = StructType(
-            [
-                StructField(
-                    self.getOrDefault("inputCol"),
-                    self.getOrDefault("inputColType"),
-                )
-            ]
-        )
-        self.set_input_schema(schema)
-
-    def count_clause(self):
-        return when(col(self.getOrDefault("inputCol")) == 1, True)
-
-    def pre_op(self, dataset):
-        return dataset
-
-    def post_op(self, dataset):
-        return dataset
-
-
 class TestingFeatureGenerator(GroupbyTransformer):
     """
     Base Implementation of the TestingFeatureGenerator.
@@ -250,6 +257,17 @@ result_df = fg.transform(ufg_df)
 def test_counter_feature():
     r_test = result_df.select("COUNT_AUTH_ACCEPT")
     a_test = ans_df.select("COUNT_AUTH_ACCEPT")
+
+    # Size test
+    assert r_test.count() == a_test.count()
+
+    # content test
+    assert r_test.subtract(a_test).count() == 0
+
+
+def test_distinct_counter_feature():
+    r_test = result_df.select("COUNT_UNIQUE_EVENTS")
+    a_test = ans_df.select("COUNT_UNIQUE_EVENTS")
 
     # Size test
     assert r_test.count() == a_test.count()
@@ -335,17 +353,6 @@ def test_max_time_bt_records():
     assert r_test.subtract(a_test).count() == 0
 
 
-# def test_min_user_timestamp():
-#     r_test = result_df.select("MIN_USER_TIMESTAMP")
-#     a_test = ans_df.select("MIN_USER_TIMESTAMP")
-#
-#     # Size test
-#     assert r_test.count() == a_test.count()
-#
-#     # content test
-#     assert r_test.subtract(a_test).count() == 0
-
-
 def test_avg_time_bt_records():
     r_test = result_df.select("AVG_TIME_BT_RECORDS")
     a_test = ans_df.select("AVG_TIME_BT_RECORDS")
@@ -371,17 +378,6 @@ def test_std_bt_records():
 def test_user_num_acc_same_ips():
     r_test = result_df.select("USER_NUM_OF_ACCOUNTS_LOGIN_WITH_SAME_IPS")
     a_test = ans_df.select("USER_NUM_OF_ACCOUNTS_LOGIN_WITH_SAME_IPS")
-
-    # Size test
-    assert r_test.count() == a_test.count()
-
-    # content test
-    assert r_test.subtract(a_test).count() == 0
-
-
-def test_distinct_counter_feature():
-    r_test = result_df.select("COUNT_AUTH_ACCEPT")
-    a_test = ans_df.select("COUNT_AUTH_ACCEPT")
 
     # Size test
     assert r_test.count() == a_test.count()
