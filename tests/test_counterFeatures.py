d6bfc1a4da30d4d4c8d694e6f22968e81fd2da05
Andrew <andrewpoky@gmail.com>
2021-12-21 14:32:34 +0000

    added another test featureTest_improvements

--- tests/test_counterFeatures.py
+++ tests/test_counterFeatures.py
@@ -87,3 +87,55 @@ def test_count_auth_accept():
     Test for feature functionality with specified output column
     """
     assert result_df.collect()[0][1] == 0
+
+
+def test_count_auth_reject():
+
+    result_df = ft.CountAuthReject().get_transformer(["CN"]).transform(ufg_df)
+    """
+    Test for default feature functionality
+    """
+    assert result_df.collect()[0][1] == 0
+
+    """
+    Test for valid input column name (if name exists in input dataframe)
+    """
+    with raises(ValueError):
+        ft.CountAuthReject(inputCol="testin").get_transformer(
+            ["CN"]
+        ).transform(ufg_df)
+
+    test_df = ufg_df.withColumn("testin", ufg_df["SM_EVENTID"]).drop(
+        "SM_EVENTID"
+    )
+    result_df = (
+        ft.CountAuthReject(inputCol="testin")
+        .get_transformer(["CN"])
+        .transform(test_df)
+    )
+    """
+    Test for input column name change functionality
+    """
+    assert result_df.collect()[0][1] == 0
+
+    result_df = (
+        ft.CountAuthAccept(outputCol="testout")
+        .get_transformer(["CN"])
+        .transform(ufg_df)
+    )
+
+    """
+    Test for output column name change functionality
+    """
+    assert "testout" in result_df.columns
+
+    """
+    Test for correct number of rows in result dataframe with specified output 
+    column
+    """
+    assert result_df.count() == 1
+
+    """
+    Test for feature functionality with specified output column
+    """
+    assert result_df.collect()[0][1] == 0
