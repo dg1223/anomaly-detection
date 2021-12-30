332d2785687d4a1597142243a84fa499d685e0c9
Ketan Bhalerao <ketan.bhalerao1450@gmail.com>
2021-12-30 15:07:32 -0500

    Added json_storage.py

---
+++ tests/json_storage.py
@@ -0,0 +1,15 @@
+import json
+import pyspark
+
+
+def store_json(df: pyspark.sql.DataFrame, storage_path):
+    data_map = map(lambda row: row.asDict(), df.collect())
+    data = [x for x in data_map]
+
+    schema = [json.loads(df.schema.json())]
+
+    json_dict = {'data': data, 'schema': schema}
+    with open(storage_path, "w") as outfile:
+        json.dump(json_dict, outfile, default=str)
+
+    return True
